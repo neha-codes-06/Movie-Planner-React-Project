1b180b5d910ba893ec0857c85ef82d8245b1ddc1
Neha <nehasundriyal0605@gmail.com>
2026-07-08 11:46:26 +0530

    add

---
+++ src/components/MovieModal.jsx
@@ -0,0 +1,10 @@
+function MovieModal(){
+    return(
+        <div className="modal">
+            <div className="modal-content">
+                <h2>Movie details</h2>
+
+            </div>
+        </div>
+    )
+}
